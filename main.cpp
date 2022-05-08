371655101ef1a5bd3f8f995a7515a7a1156ffb76
TrevorHProgramming <88110967+TrevorHProgramming@users.noreply.github.com>
2022-05-08 18:15:40 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     ProgWindow w;
     w.setWindowTitle("TF2004 Model and Texture Converter");
+    w.resize(1200, 600);
     w.show();
     return a.exec();
 }
